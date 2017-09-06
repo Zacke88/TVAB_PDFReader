912046323b639fcc6ab0f49b5a70270c3f0a7fb5
Zacke88 <zakrisson88@gmail.com>
2017-09-06 16:27:01 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        PDFManager pdf = new PDFManager();
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/PDFManager.java
@@ -0,0 +1,54 @@
+/**
+ * Created by zakri on 2017-09-06.
+ */
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class PDFManager {
+
+    String path = "";
+    String filename = "";
+
+
+    public PDFManager() {
+        try {
+            readPDF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readPDF() throws IOException {
+        try (PDDocument document = PDDocument.load(new File((path + "/" + filename)))) {
+
+            document.getClass();
+
+            if (!document.isEncrypted()) {
+
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                PDFTextStripper tStripper = new PDFTextStripper();
+
+                String pdfFileInText = tStripper.getText(document);
+                //System.out.println("Text:" + st);
+
+                // split by whitespace
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    System.out.println(line);
+                }
+
+            }
+        }
+
+
+    }
+}
